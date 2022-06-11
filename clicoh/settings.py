a683459c56e43712e290917dd512338396496dc8
Gonzalo Medrano <gonzalomedrano09@gmail.com>
2022-06-11 00:13:23 -0300

    Fix .env file path

--- clicoh/settings.py
+++ clicoh/settings.py
@@ -14,11 +14,11 @@ import os
 import environ
 
 
-env = environ.Env()
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
