dc6a9690edf836b24cd44cd88492904a67463bf6
Gonzalo Medrano <gonzalomedrano09@gmail.com>
2022-06-12 03:35:03 -0300

    Add order view

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from api.views.product_view import ProductViewSet
+from api.views.order_view import OrderViewSet
 
 
 router = DefaultRouter()
 
 router.register(r"products", ProductViewSet, basename="products")
+router.register(r"orders", OrderViewSet, basename="orders")
 
 urlpatterns = router.urls
---
+++ api/views/order_view.py
@@ -0,0 +1,22 @@
+from rest_framework import viewsets
+from api.models.order import Order
+from api.serializers.order_serializers.order_serializer import OrderSerializer
+from api.serializers.order_serializers.order_update_serializer import OrderUpdateSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+
+    queryset = Order.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ["create", "retrieve", "list"]:
+            return OrderSerializer
+        elif self.action in ["update", "partial_update"]:
+            return OrderUpdateSerializer
+
+    def perform_destroy(self, instance):
+        for order_detail in instance.order_details:
+            order_detail.product += order_detail.quantity
+            order_detail.product.save()
+        instance.products.clear()
+        instance.delete()
