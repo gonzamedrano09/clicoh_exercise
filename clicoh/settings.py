6b8b3d787d1dd3ba2186f110cbc72190ec0a9c32
Gonzalo Medrano <gonzalomedrano09@gmail.com>
2022-06-15 16:59:32 -0300

    Add djangorestframework-simplejwt, set some settings to handle jwt's, set default permission policy

--- clicoh/settings.py
+++ clicoh/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 import environ
+from datetime import timedelta
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -133,6 +134,20 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # djangorestframework
 
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated"
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10
+}
+
+
+# djangorestframework-simplejwt
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7)
 }
